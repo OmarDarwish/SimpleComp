db1766a2b329b64c472479ea235a9caa7dd80452
Omar Darwish <om.drwsh@gmail.com>
2014-02-23 20:29:59 -0600

    control functions defined

--- CPU.cpp
+++ CPU.cpp
@@ -1,1 +1,292 @@
+#include <iostream>
+#include <string>
+#include <limits>
+
 #include "CPU.h"
+
+using namespace std;
+
+CPU::CPU(int threshold){
+	InterruptTimerThreshold = threshold;
+	PC = 0;
+	SP = 0;
+	IR = 0;
+	AC = 0;
+	X = 0;
+	Y = 0;
+	lastInt = 0;
+}
+
+int CPU::fetch(){
+	return fetch(PC);
+}
+
+int CPU::fetch(int addr){
+	//clear out all other input
+	cin.ignore(numeric_limits<streamsize>::max());
+	//write to stdout the addr that needs be retrieved
+	cout << addr << endl;
+	
+	//wait until memory responds to stdin
+	cin >> IR;
+	PC++;
+
+	return IR;
+}
+
+void write(int addr, int val){
+	cout << addr << " " << val << endl;
+}
+
+void CPU::execute(int inst){
+	switch(inst){
+		case LOADVAL:
+			loadVal(inst);
+			break;
+
+		case LOADADDR:
+			loadAddr(inst);
+			break;
+
+		case LOADINDADDR:
+			loadIndAddr(inst);
+			break;
+
+		case LOADIDXXADDR:
+			loadIdxXaddr(inst);
+			break;
+
+		case LOADIDXYADDR:
+			loadIdxYAddr(inst);
+			break;
+
+		case LOADSPX:
+			loadSpX();
+			break;
+
+		case STOREADDR:
+			storeAddr(inst);
+			break;
+
+		case GETRAND:
+			getRand();
+			break;
+
+		case PUTPORT:
+			putPort(inst);
+			break;
+
+		case ADDX:
+			addX();
+			break;
+
+		case ADDY:
+			addY();
+			break;
+
+		case SUBX:
+			subX();
+			break;
+
+		case SUBY:
+			subY();
+			break;
+
+		case COPYTOX:
+			copyToX();
+			break;
+
+		case COPYFROMX:
+			copyFromX();
+			break;
+
+		case COPYTOY:
+			copyToY();
+			break;
+
+		case COPYFROMY:
+			copyFromY();
+			break;
+
+		case COPYTOSP:
+			copytoSp();
+			break;
+
+		case COPYFROMSP:
+			copyFromSp();
+			break;
+
+		
+		case CALLADDR:
+			callAddr();
+			break;
+
+		case RET:
+			ret();
+			break;
+
+		case INCX:
+			incX();
+			break;
+
+		case PUSH:
+			push();
+			break;
+
+		case POP:
+			pop();
+			break;
+
+		case INTERRUPT:			
+			interrupt();
+			break;
+
+		case INTERRUPTRET:		
+			interruptRet();
+			break;
+
+		case END:		
+			end();
+			break;
+
+		case JUMPADDR:			
+			jumpAddr(inst);
+			break;
+
+		case JUMPIFEQLADDR:		
+			jumpIfEqlAddr(inst);
+			break;
+
+		case JUMPIFNOTEQUALADDR:		
+			jumpIfNotEqualAddr(inst);
+			break;
+
+		default:
+			terminate();
+			break;
+	}
+}
+
+void CPU::loadVal(int val){
+
+}
+
+void CPU::loadAddr(int addr){
+
+}
+
+void CPU::loadIndAddr(int addr){
+
+}
+
+void CPU::loadIdxXaddr(int addr){
+
+}
+
+void CPU::loadIdxYAddr(int addr){
+
+}
+
+void CPU::loadSpX(){
+
+}
+
+void CPU::storeAddr(int addr){
+
+}
+
+void CPU::getRand(){
+
+}
+
+void CPU::putPort(int port){
+
+}
+
+void CPU::addX(){
+
+}
+
+void CPU::addY(){
+
+}
+
+void CPU::subX(){
+
+}
+
+void CPU::subY(){
+
+}
+
+void CPU::copyToX(){
+
+}
+
+void CPU::copyFromX(){
+
+}
+
+void CPU::copyToY(){
+
+}
+
+void CPU::copyFromY(){
+
+}
+
+void CPU::copytoSp(){
+
+}
+
+void CPU::copyFromSp(){
+
+}
+
+void CPU::jumpAddr(int addr){
+
+}
+
+void CPU::jumpIfEqlAddr(int addr){
+
+}
+
+void CPU::jumpIfNotEqualAddr(int addr){
+
+}
+
+void CPU::callAddr(){
+
+}
+
+void CPU::ret(){
+
+}
+
+void CPU::incX(){
+
+}
+
+void CPU::push(){
+
+}
+
+void CPU::pop(){
+
+}
+
+void CPU::interrupt(){
+
+} 
+
+void CPU::interruptRet(){
+
+} 
+
+void CPU::end(){
+
+}
+
+void CPU::terminate(){
+
+}
--- CPU.h
+++ CPU.h
@@ -13,50 +13,53 @@ class CPU{
 public:
 	enum Instructions	
 	{
-		LoadVal = 1,				//Load value into AC
-		LoadAddr = 2,				//Load the value at the address into the AC
-		LoadIndAddr = 3,			//Load the value from the address found in the address specified into AC
-		LoadIdxXAddr = 4,			//Load the value at (address + X) into the AC
-		LoadIdxYAddr = 5,			//Load the value at (address + Y) into the AC
-		LoadSpX = 6,				//Load from (SP + X) into the AC
-		StoreAddr = 7,				//Store the value in AC into the address specified
-		GetRand = 8,				//Gets a random int from 1 to 100 inclusive to the AC
-		PutPort = 9,				//If port = 1, writes AC as an int to the screen
+		LOADVAL = 1,				//Load value into AC
+		LOADADDR = 2,				//Load the value at the address into the AC
+		LOADINDADDR = 3,			//Load the value from the address found in the address specified into AC
+		LOADIDXXADDR = 4,			//Load the value at (address + X) into the AC
+		LOADIDXYADDR = 5,			//Load the value at (address + Y) into the AC
+		LOADSPX = 6,				//Load from (SP + X) into the AC
+		STOREADDR = 7,				//Store the value in AC into the address specified
+		GETRAND = 8,				//Gets a random int from 1 to 100 inclusive to the AC
+		PUTPORT = 9,				//If port = 1, writes AC as an int to the screen
 									//If port = 2, writes AC as a char to the screen
 
-		AddX = 10,					//Add the value in X to the AC
-		AddY = 11,					//Add the value in X to the AC
-		SubX = 12,					//Subtract the value in X from the AC
-		SubY = 13,					//Subtract the value in Y from the AC
-		CopyToX = 14,				//Copy the value in the AC to X
-		CopyFromX = 15,				//Copy the value in X to the AC
-		CopyToY = 16,				//Copy the value in the AC to Y
-		CopyFromY = 17,				//Copy the value in Y to the AC
-		CopyToSp = 18,				//Copy the value in AC to the SP
-		CopyFromSp = 19,			//Copy the value in SP to the AC
+		ADDX = 10,					//Add the value in X to the AC
+		ADDY = 11,					//Add the value in X to the AC
+		SUBX = 12,					//Subtract the value in X from the AC
+		SUBY = 13,					//Subtract the value in Y from the AC
+		COPYTOX = 14,				//Copy the value in the AC to X
+		COPYFROMX = 15,				//Copy the value in X to the AC
+		COPYTOY = 16,				//Copy the value in the AC to Y
+		COPYFROMY = 17,				//Copy the value in Y to the AC
+		COPYTOSP = 18,				//Copy the value in AC to the SP
+		COPYFROMSP = 19,			//Copy the value in SP to the AC
 
-		JumpAddr = 20,				//Jump to the address
-		JumpIfEqlAddr= 21,			//Jump to the address if the value in AC is equal to 0
-		JumpIfNotEqualAddr = 22,	//Jump to the address if the value in AC is not equal to 0
-		CallAddr = 23,				//Push return address onto stack, jump to address
-		Ret = 24,					//Pop return address from the stack, jump to the address
-		IncX = 25,					//Increment the value in X
-		DecX = 26,					//Decrement the value in X
+		JUMPADDR = 20,				//Jump to the address
+		JUMPIFEQLADDR= 21,			//Jump to the address if the value in AC is equal to 0
+		JUMPIFNOTEQUALADDR = 22,	//Jump to the address if the value in AC is not equal to 0
+		CALLADDR = 23,				//Push return address onto stack, jump to address
+		RET = 24,					//Pop return address from the stack, jump to the address
+		INCX = 25,					//Increment the value in X
+		DECX = 26,					//Decrement the value in X
 
-		Push = 27,					//Push AC onto the stack
-		Pop = 28,					//Pop from stack into AC
-		Int = 29,					//Set system mode, switch stack, push SP and PC, set new SP and PC
-		IRet = 30,					//Restore registersm set user mode
-		End = 50					//End Executuion
+		PUSH = 27,					//Push AC onto the stack
+		POP = 28,					//Pop from stack into AC
+		INTERRUPT = 29,					//Set system mode, switch stack, push SP and PC, set new SP and PC
+		INTERRUPTRET = 30,					//Restore registersm set user mode
+		END = 50					//End Executuion
 	};
 
 	CPU(int threshold = 5);			//default interrupt timer threshold
+	void begin();					//start execution
 	void execute(int inst);			//passes instruction to its specified handler
-	void execute(int inst, int arg); //passes instruction to its specified handler with arg
 
 protected:
-	int fetch();					//fetches instruction from memory process
+	int fetch();					//fetches at PC location
+	int fetch(int addr);			//fetches instruction from memory process
+	void write(int addr, int val);	//writes val to memory address
 	void terminate();				//error termination
+
 
 	//instruction handlers
 	void loadVal(int val);
@@ -66,7 +69,7 @@ protected:
 	void loadIdxYAddr(int addr);
 	void loadSpX();
 	void storeAddr(int addr);
-	int getRand();
+	void getRand();
 	void putPort(int port);
 	void addX();
 	void addY();
@@ -84,7 +87,6 @@ protected:
 	void callAddr();
 	void ret();
 	void incX();
-	void incY();
 	void push();
 	void pop();
 	void interrupt(); 
@@ -95,7 +97,7 @@ protected:
 private:
 	int PC, SP, IR, AC, X, Y; //registers
 	int InterruptTimerThreshold; //num instructions before interrupt 
-	int InstrSinceLastInt; //num instructions since last interrupt occured.
+	int lastInt; //num instructions since last interrupt occured.
 };
 
 #endif //CPU_H
